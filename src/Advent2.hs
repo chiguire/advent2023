a1a0062070cff3d38d9dee0c73dfe2380501fca6
Ciro Duran <ciro.duran@gmail.com>
2023-12-04 00:11:33 +0000

    advent3_1

--- src/Advent2.hs
+++ src/Advent2.hs
@@ -3,8 +3,6 @@ module Advent2
     ( advent2_1, advent2_2
     ) where
 
-import Control.Arrow
-
 import Text.Heredoc
 
 import Text.Parsec
@@ -22,7 +20,7 @@ advent2_2 = (sum . map (getGamePower . foldr (getMins) (Reveal 0 0 0) . getGameR
 
 parseInput :: Parser [Game]
 parseInput = do
-    games <- manyTill parseGame eof
+    games <- many1 parseGame
     return games
 
 parseGame :: Parser Game
@@ -31,7 +29,7 @@ parseGame = do
     gameId <- read <$> many1 digit
     string ": "
     reveals <- parseReveals
-    endOfLine
+    try endOfLine <|> (eof >> return ' ')
     return $ Game gameId reveals
 
 parseReveals :: Parser [Reveal]
@@ -88,8 +86,7 @@ example_input = [here|Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-|]
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green|]
 
 input = [here|Game 1: 1 red, 5 blue, 1 green; 16 blue, 3 red; 6 blue, 5 red; 4 red, 7 blue, 1 green
 Game 2: 4 blue; 4 red, 3 blue, 1 green; 4 red, 9 blue, 2 green; 5 blue, 7 green, 4 red
@@ -190,5 +187,4 @@ Game 96: 7 blue, 6 green, 2 red; 3 green, 1 blue; 7 blue, 3 red, 5 green; 1 gree
 Game 97: 10 red, 1 green, 1 blue; 4 green, 11 red, 2 blue; 4 red, 1 blue, 4 green
 Game 98: 3 green, 4 blue, 7 red; 7 red, 8 green; 7 green, 16 red, 1 blue; 8 green, 2 blue, 4 red; 5 green, 3 blue, 18 red
 Game 99: 6 green, 12 red, 1 blue; 5 blue, 1 red, 7 green; 5 green, 7 red, 10 blue; 8 blue, 1 red, 7 green; 17 red, 4 blue, 9 green
-Game 100: 6 blue, 10 green; 3 green, 4 blue, 1 red; 7 blue, 1 red, 12 green
-|]
+Game 100: 6 blue, 10 green; 3 green, 4 blue, 1 red; 7 blue, 1 red, 12 green|]
